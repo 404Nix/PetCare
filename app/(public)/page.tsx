8fc7de6e1edc53896bcc7f0b0cee653edfa5d2ba
Nikhil <nikhilkanojia693@gmail.com>
2026-07-23 01:10:30 +0530

    add Initial folder structure

---
+++ app/(public)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center text-7xl'>page</div>
+  )
+}
+
+export default page
