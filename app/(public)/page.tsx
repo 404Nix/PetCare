dfde0a1f993c1854f792888f0ca7cd612e735b77
Nikhil <nikhilkanojia693@gmail.com>
2026-07-26 10:08:50 +0530

    add required packages

--- app/(public)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div className='w-full h-screen flex justify-center items-center text-7xl'>page</div>
-  )
-}
-
-export default page
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <h1 className="h-screen text-5xl flex justify-center items-center">
+            Hellow bro
+        </h1>
+    );
+}
