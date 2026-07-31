01676d24acafd843368b81a4741403505198e05d
Nikhil <nikhilkanojia693@gmail.com>
2026-07-31 21:59:54 +0530

    fix file structure

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,51 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import StoreProvider from "@/components/StoreProvider";
 import Navbar from "@/components/LandingPage/Navbar";
 import Footer from "@/components/LandingPage/Footer";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-    title: "PetCare",
-    description: "PetCare - Care you pets",
-};
-
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html
-            lang="en"
-            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-        >
-            <body className="min-h-full flex flex-col">
-                <StoreProvider>
-                    <ClerkProvider>
-                        <header>
-                            <Navbar />
-                        </header>
-
-                        {children}
-                        <footer>
-                            <Footer />
-                        </footer>
-
-                    </ClerkProvider>
-                </StoreProvider>
-            </body>
-        </html>
+        <>
+            <header><Navbar /></header>
+            <main>{children}</main>
+            <footer><Footer /></footer>
+        </>
     );
 }
